15bc96c0d10a0bbab6c12686525749c558146712
James P Bondeson <jpbond58@gmail.com>
2020-07-09 01:29:30 +0000

    Automatically backed up by Learn

---
+++ sandbox.rb
@@ -0,0 +1,12 @@
+puts "How old are you?"
+user_input = Integer(gets.chomp)
+
+if user_input<30
+  puts "Youngin'!"
+
+else user_input>30
+  puts "My condolences, old"
+
+end
+
+puts "Your age is #{user_input}"
